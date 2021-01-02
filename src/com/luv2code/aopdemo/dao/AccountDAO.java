70767566181a04925511eb9a132a80f76d5c0c19
Nitai Charan <nitaicharan@gmail.com>
2021-01-02 11:24:58 -0300

    333. AOP: Pointcut Expressions - Match Method Any Params - Match Method in a Package

--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -12,4 +12,15 @@ public class AccountDAO {
 		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
 		
 	}
+	
+	public boolean doWork() {
+		
+		System.out.println(getClass() + ": doWork()");
+		return false;
+	}
 }
+
+
+
+
+
